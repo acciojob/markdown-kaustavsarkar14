a3567816438fe06da7c22695fd76f3cac4c69de0
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-20 20:41:52 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
+const App = () => {
+    const [text, setText] = useState('')
+    return (
+        <div className='app' >
+            <textarea value={text} onChange={e => setText(e.target.value)} name="text" id='text' rows="10"></textarea>
+            <div className="preview">
+                <ReactMarkdown>
+                    {text}
+                </ReactMarkdown>
+            </div>
+        </div>
+    )
+}
+
+export default App
